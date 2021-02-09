3f2320ad631e2f2eb7b3b50b31acb99938803aba
windshaky <windshaky@users.noreply.github.com>
2021-02-09 17:30:33 +0900

    미배달 사유 일괄전송 추가

--- WebContent/contents/COM/js/COM0701.js
+++ WebContent/contents/COM/js/COM0701.js
@@ -9,10 +9,10 @@ var page = {
 		/**
 		 * 이전 페이지에 따른 분기 처리
 		 */
-		
+
 		// 이전페이지에서 전달받은 파라미터를 전역변수에 저장
 		page.com0701=arg.data.param;
-		
+
 		// 이전 페이지의 id값을 변수에 저장
 		var menu = arg.data.param.menu_id;
 		switch (menu) {
@@ -35,6 +35,8 @@ var page = {
 //			page.com0701.typ_cd = "UPICK_RSN_CD";
 //			page.com0701.menu_title = "미집하처리 사유 등록";
 //			break;
+		// 미배달사유 일괄전송
+		case "CLDL0701":
 		// 배달완료에서 넘어온 경우
 		case "CLDL0401":
 			page.com0701.typ_cd = "UDLV_RSN_CD";
@@ -43,17 +45,17 @@ var page = {
 		default:
 			break;
 		}
-		
+
 		// headerWrap 클래스명을 지닌 div 하위의 h1에 이전 페이지의 메뉴이름을 지정
 		$("div[class='headerWrap'] > h1").text(page.com0701.menu_title);
-		
+
 		page.initInterface();
 	}
 	,apiParam : {
 			id:"HTTP",			// 디바이스 콜 id
 			param:{				// 디바이스가 알아야할 데이터
 				task_id : "",										// 화면 ID 코드가 들어가기로함
-				//position : {},									// 사용여부 미확정 
+				//position : {},									// 사용여부 미확정
 				type : "",
 				baseUrl : "",
 				method : "POST",									// api 호출 형식(지정 안하면 'POST' 로 자동 셋팅)
@@ -71,8 +73,8 @@ var page = {
 		$(".btn.closeW.paR").click(function(){
 			LEMP.Window.close();
 		});
-		
-		
+
+
 		// 라디오 li 클릭
 		$(document).on("click",".radio > li",function(){
 			$(this).find("input").prop("checked",true);
@@ -84,12 +86,13 @@ var page = {
 				$(".raView").hide();
 			}
 		})
-		
-		
+
+
 		// 확인 버튼 클릭시
 		$(".btn.red.w100p.m").click(function(){
 			// 넘길 데이터 생성
 			var code = $("input[name=ra]:checked").attr("id");
+			var name = $("input[name=ra]:checked").labels().text();
 			var obj = {
 					"corp_sct_cd":page.com0701.corp_sct_cd,
 					"cldl_sct_cd":page.com0701.cldl_sct_cd,
@@ -97,10 +100,11 @@ var page = {
 					"rsrv_mgr_no":page.com0701.rsrv_mgr_no,	// 카카오 집하보류 접수번호사용
 					"code":code,
 					"value":undefined,
-					"images":undefined
+					"images":undefined,
+					"name":name
 			}
-			
-			if (code ==="99" || (code==="17"&& page.com0701.typ_cd === "UPICK_RSN_CD") 
+
+			if (code ==="99" || (code==="17"&& page.com0701.typ_cd === "UPICK_RSN_CD")
 					|| (code==="26"&& page.com0701.typ_cd === "UDLV_RSN_CD")) {
 				obj.value = $("#"+code).val();
 			}else if(code === "06" && page.com0701.corp_sct_cd == "2201"){	//카카오의 기타(06)일경우
@@ -108,14 +112,14 @@ var page = {
 			}else if (code==="22" && page.com0701.typ_cd === "UPICK_RSN_CD") {
 				obj.images = $("#"+code).val();
 			}
-			
+
 			// 라디오 버튼을 선택하지 않았을 경우
 			if (!$("input[name=ra]").is(":checked")){
 				LEMP.Window.alert({
 					"_sTitle" : "알림",
 					"_vMessage" : "버튼을 선택해주세요."
 				});
-				
+
 			// 기타를 선택했을 경우
 			}else if (code ==="99" && smutil.isEmpty($("#"+code).val())) {
 				LEMP.Window.alert({
@@ -128,17 +132,17 @@ var page = {
 					"_sTitle" : "알림",
 					"_vMessage" : "사유가 입력되지 않았습니다."
 				});
-				
-			// 지정일회수, 지정일배송을 선택하고 날짜를 지정하지 않은경우 
-			} else if ( 
+
+			// 지정일회수, 지정일배송을 선택하고 날짜를 지정하지 않은경우
+			} else if (
 						(
-							code ==="17" 
-							&& page.com0701.typ_cd === "UPICK_RSN_CD" 
+							code ==="17"
+							&& page.com0701.typ_cd === "UPICK_RSN_CD"
 							&& smutil.isEmpty($("#"+code).val())
 						) ||
 						(
-							code ==="26" 
-							&& page.com0701.typ_cd === "UDLV_RSN_CD" 
+							code ==="26"
+							&& page.com0701.typ_cd === "UDLV_RSN_CD"
 							&& smutil.isEmpty($("#"+code).val())
 						)
 			){
@@ -146,19 +150,19 @@ var page = {
 					"_sTitle" : "알림",
 					"_vMessage" : "지정일이 선택되지 않았습니다."
 				});
-				
+
 			// 미집하처리 취급불가를 이미지 없이 확인했을 경우
 			}else if (
-						code === "22" 
-						&& smutil.isEmpty($("#"+code).val()) 
+						code === "22"
+						&& smutil.isEmpty($("#"+code).val())
 						&& page.com0701.typ_cd === "UPICK_RSN_CD"
 			){
 				LEMP.Window.alert({
 					"_sTitle" : "알림",
 					"_vMessage" : "이미지가 선택되지 않았습니다."
 				});
-				
-			} 
+
+			}
 			else{
 				LEMP.Window.close({
 					"_oMessage":{
@@ -168,8 +172,8 @@ var page = {
 				});
 			}
 		});
-		
-		
+
+
 		// 카메라버튼 클릭
 		// 취급불가 코드를 선택후 사진이 있어야하게끔..
 		$(document).on("click",".btn.camera2.fr",function(){
@@ -184,8 +188,8 @@ var page = {
 				}
 			})
 		});
-		
-		
+
+
 		// 달력버튼 클릭
 		$(document).on("click",".inCal",function(){
 			var popUrl = smutil.getMenuProp("COM.COM0301","url");
@@ -193,7 +197,7 @@ var page = {
 				"minDate":1,
 				"maxDate":3
 			}
-			
+
 			LEMP.Window.open({
 				"_sPagePath":popUrl,
 				"_oMessage":{
@@ -201,8 +205,8 @@ var page = {
 				}
 			});
 		});
-		
-		
+
+
 		// 기타 li 클릭
 		$(document).on("click","#99",function(){
 			var popUrl = smutil.getMenuProp("COM.COM0602","url");
@@ -210,7 +214,7 @@ var page = {
 				"_sPagePath":popUrl
 			})
 		});
-		
+
 		// 카카오 기타 li 클릭
 		$(document).on("click","#06",function(){
 			if(page.com0701.corp_sct_cd == "2201"){
@@ -219,18 +223,18 @@ var page = {
 					"_sPagePath":popUrl
 				})
 			}
-		});		
-		
+		});
+
 		Handlebars.registerHelper('showRaView', function(options) {
 			// 미집하처리의 지정일회수이면 날짜 인풋 추가
 			if((page.com0701.typ_cd=='UPICK_RSN_CD' && this.dtl_cd == '17')
 				//미배달처리의 지정일배송이면 날짜 인풋 추가
 				|| (page.com0701.typ_cd=='UDLV_RSN_CD' && this.dtl_cd == '26')){
-				
+
 				var html = '<div class="raView dsn mgt10 pdl25">';
 				html += '<input type="text" value="" name="" placeholder="" class="inCal" id="inCal" readonly="readonly">';
 				html += '</div>';
-				
+
 				return new Handlebars.SafeString(html); // mark as already escaped
 			//미배달처리의 취급불가이면 카메라 버튼 추가
 			}else if (page.com0701.typ_cd=="UPICK_RSN_CD"&& this.dtl_cd=="22") {
@@ -238,31 +242,37 @@ var page = {
 				return new Handlebars.SafeString(html); // mark as already escaped
 			}
 		});
-		
-		
+
+
 		// 리스트 출력
 		/**
 		 * 전역변수 com0701의 typ_cd 값을 codeListPopup api의 파라미터로 조회합니다.
 		 */
 		var data ={"typ_cd":page.com0701.typ_cd};
 		page.codeListPopup(data);
-		
+
 	}
 	,codeListPopup:function(data){
 		smutil.loadingOn();		// 로딩바 열기
-		
+
 		page.apiParam.param.baseUrl = "smapis/cmn/codeListPopup";
 		page.apiParam.param.callback = "page.codeListPopupCallback";
 		page.apiParam.data.parameters = data;
-		
-		// 공통 api호출 함수 
+
+		// 공통 api호출 함수
 		smutil.callApi(page.apiParam);
 	}
 	,codeListPopupCallback:function(res){
 		try{
 			if (smutil.apiResValidChk(res) && res.code==="0000") {
+				//8시30분 이전인경우 심야배송(42) 노출하지 않음
+				if(page.com0701.typ_cd = "UDLV_RSN_CD" && !smutil.isMidnight()){
+					const idx = res.data.list.findIndex(function(item) {return item.dtl_cd === "42"})
+					if (idx > -1) res.data.list.splice(idx, 1)
+				}
+
 				// 가져온 핸들바 템플릿 컴파일
-				var template = Handlebars.compile($("#com0701_list_template").html()); 
+				var template = Handlebars.compile($("#com0701_list_template").html());
 				// 핸들바 템플릿에 데이터를 바인딩해서 생성된 HTML을 DOM에 주입
 				$('#com0701LstUl').append(template(res.data));
 			}else {
@@ -278,13 +288,13 @@ var page = {
 			smutil.loadingOff();			// 로딩바 닫기
 		}
 	}
-//	
-	
+//
+
 	//com0301에서 날짜 선택 한 후 실행되는 콜백 함수
 	,COM0301Callback:function(res){
 		page.com0701.value=res.param.date.replace(/\./g,'');
 		$("#inCal").val(res.param.date);
-		
+
 		if($("#17").length>0 && page.com0701.typ_cd === "UPICK_RSN_CD"){
 			$("#17").val(res.param.date.replace(/\./g,''));
 		}
