f3471408f7455ef7f4575eaab4d95fc092b734a2
junhyeong <wnsgud0o@naver.com>
2022-09-23 11:32:01 +0900

    집하완료 옵션  도착지 검색 변경

--- WebContent/contents/CLDL/js/CLDL0306.js
+++ WebContent/contents/CLDL/js/CLDL0306.js
@@ -32,9 +32,35 @@ var page = {
 			"parameters" : {}
 		}
 	}
+	// api 파람메터 초기화 
+	,apiParamInit : function(){
+		page.apiParam =  {
+			id:"HTTP",			// 디바이스 콜 id
+			param:{				// 디바이스가 알아야할 데이터
+				task_id : "",										// 화면 ID 코드가 들어가기로함
+				//position : {},									// 사용여부 미확정 
+				type : "",
+				baseUrl : "",
+				method : "POST",									// api 호출 형식(지정 안하면 'POST' 로 자동 셋팅)
+				callback : "",					// api 호출후 callback function
+				contentType : "application/json; charset=utf-8"
+			},
+			data:{"parameters" : {}}// api 통신용 파라메터
+		};
+	}
 	,cldl0306:{}
 	,initInterface : function()
 	{
+		//마이크 버튼
+		$("#micBtn").click(function(){
+			page.callStt();
+		});
+
+		// ios 음성인식 완료버튼을 클릭한경우 
+		$(".btn.gray3.m.w100p.b-close").click(function(){
+			page.callSttIos();
+		});
+		
 		// 닫기 버튼
 		$(".btn.closeW.paR").click(function(){
 			LEMP.Window.close();
@@ -238,8 +264,8 @@ var page = {
 			$("#cldl0306Ul").empty();
 		})
 		
-		// 검색버튼 클릭
-		$("#addrSearch").click(function(){
+		// 검색버튼 클릭(구)
+/*		$("#addrSearch").click(function(){
 			var data = {};
 			var check_sido = $("#cldl0306SidoSel option:selected");
 			var check_gungu = $("#cldl0306GunguSel option:selected");
@@ -329,8 +355,71 @@ var page = {
 				}
 			}
 			
+		});*/
+		// 검색버튼 클릭(신)
+		$("#addrSearch").click(function(){
+			var data = {};
+			
+			var addr = $("#addr_input").val();
+			var network = $("input[type=radio][id$=SVC_CD]:checked").val();
+			
+			var zip_no_input = ($("#zip_no_input").val()).replace(/\-/gi,"");
+			var area_no;		//새우편번호
+			var zip_no;			//구우편번호
+			
+			if(zip_no_input.length == 5){
+				area_no = zip_no_input;
+				zip_no = "";
+			}else if(zip_no_input.length == 6){
+				area_no = "";
+				zip_no = zip_no_input;
+			}else if(zip_no_input.length == 0){
+				area_no = "";
+				zip_no = "";
+			}else{
+				LEMP.Window.toast({
+					"_sMessage":"유효하지 않은 우편번호",
+					'_sDuration' : 'short'
+				});
+//				LEMP.Window.alert({
+//					"_sTitle" : "분류코드 검색",
+//					"_vMessage" : "유효하지 않은 우편번호"
+//				});
+				
+				$("#cldl0306Ul").html("");
+				return;
+			}
+			
+			if (smutil.isEmpty(addr)) {
+				LEMP.Window.toast({
+					"_sMessage":"전체 주소를 입력해주세요.",
+					'_sDuration' : 'short'
+				});
+//				LEMP.Window.alert({
+//					"_sTitle" : "분류코드 검색",
+//					"_vMessage" : "전체 주소를 입력해주세요."
+//				});
+			}else if ($("#addr_input").val().length < 2) {
+				LEMP.Window.toast({
+					"_sMessage":"전체 주소를 입력해주세요.",
+					'_sDuration' : 'short'
+				});
+//				LEMP.Window.alert({
+//					"_sTitle" : "분류코드 검색",
+//					"_vMessage" : "전체 주소를 입력해주세요."
+//				});
+			}else {
+				data = {
+					"network": network,
+					"address": addr,
+					"zip_no": zip_no,
+					"area_no": area_no
+				}
+				
+				page.addr(data);
+			}
+			
 		});
-		
 		
 		// 주소리스트 초기화
 		$(".addTab>ul>li,input[name='svc_cd']").click(function(){
@@ -422,7 +511,9 @@ var page = {
 		}
 		
 	}
-	,addr:function(data){
+	
+	//구형검색
+/*	,addr:function(data){
 		
 		smutil.loadingOn();		// 로딩바 열기
 		
@@ -434,9 +525,9 @@ var page = {
 		smutil.callApi(page.apiParam);
 	}
 	,addrCallback:function(data){
+		console.log("addr : ",data)
 		try{
 			var res = data.data;
-			
 			if (smutil.apiResValidChk(data) && data.code=="0000") {
 				
 				var template = Handlebars.compile($("#cldl0306_list_template").html());
@@ -448,8 +539,52 @@ var page = {
 		finally{
 			smutil.loadingOff();			// 로딩바 닫기
 		}
+	}*/
+	//신형 검색
+		,addr:function(data){
+		
+		smutil.loadingOn();		// 로딩바 열기
+		
+		page.apiParam.param.baseUrl="smapis/newAddress";
+		page.apiParam.param.callback="page.addrCallback";
+		page.apiParam.data.parameters=data;
+		
+		// 공통 api호출 함수 
+		smutil.callApi(page.apiParam);
 	}
-	
+	,addrCallback:function(data){
+		console.log("fdsafdsa : ",data)
+		try{
+			var res = data.api_msg;
+			
+			if (smutil.apiResValidChk(data) && data.code=="0000") {
+				
+				if(res.result === "success"){
+					
+					var template = Handlebars.compile($("#cldl0306_list_template").html());
+					$("#cldl0306Ul").html(template(res));
+				}
+				else {
+					LEMP.Window.toast({
+						"_sMessage":res.message,
+						'_sDuration' : 'short'
+					});
+//					LEMP.Window.alert({
+//						"_sTitle" : "분류코드 검색",
+//						"_vMessage" : res.message
+//					});
+					var template = Handlebars.compile($("#no_list_template").html());
+					
+					$("#cldl0306Ul").html(template(res));
+				}
+			}
+		}
+		catch(e){}
+		finally{
+			smutil.loadingOff();			// 로딩바 닫기
+		}
+	}
+		
 	,scanCallback:function(res){
 		if (res.barcode.length === 3 || res.barcode.length === 4) {
 			// 서비스 구분의 선택값을 체크
@@ -511,6 +646,91 @@ var page = {
 //			});
 		}
 	}
+		// ################### 음성인식 호출 start
+	,callStt : function(){
+		page.apiParamInit();		// 파라메터 전역변수 초기화
+		page.apiParam.id = "STT";
+		
+		// ios 기기인경우 시작과 종료 파라메터를 셋팅해 줘야함
+		if(smutil.deviceInfo == "smios"){
+			page.apiParam.param = {				// api 통신용 파라메터
+				"type" : "connect"
+			};
+		}
+		else{
+			page.apiParam.data = {				// api 통신용 파라메터
+				"parameters" : {}
+			};
+		}
+		
+		if(smutil.deviceInfo == "smios"){		// ios
+			// 종료 버튼이 있는 음성인식 바 넣기
+			$('.mpopBox.sms2').bPopup({modalClose: false});
+		}
+		else {		// android
+			// 음성인식 바 넣기
+			$('.mpopBox.sms').bPopup({modalClose: false});
+		}
+		
+		// 공통 api호출 함수
+		smutil.nativeMothodCall(page.apiParam);
+		
+		page.apiParamInit();			// 파라메터 전역변수 초기화
+		
+	}
+	// ios 음성인식 완료로직
+	,callSttIos : function(){
+		page.apiParamInit();		// 파라메터 전역변수 초기화
+		page.apiParam.id = "STT";
+
+		// ios 기기인경우 시작과 종료 파라메터를 셋팅해 줘야함
+		page.apiParam.param = {				// api 통신용 파라메터
+			"type" : "disConnect"
+		};
+		
+		// 공통 api호출 함수
+		smutil.nativeMothodCall(page.apiParam);
+		
+		page.apiParamInit();			// 파라메터 전역변수 초기화
+		
+	}
+	// 음성인식 콜백 
+	,sttCallback : function(result){
+		try{
+			
+			if(!smutil.isEmpty(result)){
+				// api 전송 성공
+				if(result.status == "true"){
+					var resultText = result.resultText;		// 검색어
+					
+					$('#addr_input').val(result.resultText);
+				}
+				else if(result.status == "false"){
+					LEMP.Window.toast({
+						"_sMessage":"음성인식에 실패했습니다.",
+						'_sDuration' : 'short'
+					});
+//					LEMP.Window.alert({
+//						"_sTitle" : "알림",
+//						"_vMessage" : "음성인식에 실패했습니다."
+//					});
+				}
+			}
+			
+		}
+		catch(e){}
+		finally{
+			
+			if(smutil.deviceInfo == "smios"){		// ios
+				$('.mpopBox.sms2').bPopup().close();
+			}
+			else {		// android
+				$('.mpopBox.sms').bPopup().close();
+			}
+			
+			page.apiParamInit();			// 파라메터 전역변수 초기화
+		}
+	},
 	
 };
 
