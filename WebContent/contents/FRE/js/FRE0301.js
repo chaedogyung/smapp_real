5705931e0a0ae698b8e7ee2434a9c220cf88fe9f
junhyeong <wnsgud0o@naver.com>
2022-12-06 16:23:08 +0900

    화물추적 카메라 자동설정 수정

--- WebContent/contents/FRE/js/FRE0301.js
+++ WebContent/contents/FRE/js/FRE0301.js
@@ -191,33 +191,15 @@ var page = {
 		
 		 $(function(){
 			page.dlvyCompl = LEMP.Properties.get({ "_sKey" : "autoMenual"});
-			if(!smutil.isEmpty(page.dlvyCompl.area_sct_cd6) &&page.dlvyCompl.area_sct_cd6 == "Y"){
-				$("#auto_camera").prop('checked', true)
-				LEMP.Window.openCodeReader({
-					"_fCallback" : function(res) {
-						if(res.result){
-							if(String(res.data).length == 12 && (Number(String((res.data)).substr(0,11))%7 ==
-										Number(String((res.data)).substr(0,11))%7)){
-								page.changeForm(res.data);
-								page.trclInfo(res.data);
-							}else{
-								LEMP.Window.alert({
-									"_sTitle" : "경고",
-									"_vMessage" : "정상적인 바코드번호가 아닙니다."
-								});
-							}
-						}else{
-							LEMP.Window.alert({
-								"_sTitle" : "경고",
-								"_vMessage" : "바코드를 읽지 못했습니다."
-							});
-						}
-					}
-				});
-			}else{
-				$("#auto_camera").prop('checked', false)
-			}			
-		});		
+			console.log("Fdsafdsa :", page.dlvyCompl)
+			if(!smutil.isEmpty(page.dlvyCompl.area_sct_cd6) &&page.dlvyCompl.area_sct_cd6 == "N"){
+				$("#setDlvyCom1").text('수동');
+				$("#setDlvyCom1").attr('class', 'gray2 badge option outline paR');
+			}else {
+				$("#setDlvyCom1").text('자동');
+        	    $("#setDlvyCom1").attr('class', 'blue badge option outline paR');
+				}			
+		});
 	},
 	
 	//송장번호 입력후 전송
