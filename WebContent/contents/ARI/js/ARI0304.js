fea97a908e6546ad1257ffe99891c9d22ecda24d
chaedogyung <host0320@naver.com>
2023-06-05 09:35:17 +0900

    소형적입 화면에 운반장비번호 스캔을 스캐너로 할 수 있게 스캔콜백 로직 추가

--- WebContent/contents/ARI/js/ARI0304.js
+++ WebContent/contents/ARI/js/ARI0304.js
@@ -30,5 +30,19 @@ var page = {
 //					});
 				}
 			});
-		}
+		},
+    	scanCallback : function(data){
+    		if(data.barcode.substr(0,1) =="8" &&
+    		    data.barcode.length == 10 &&
+    		    Number(data.barcode.substr(0,9)%7 == Number(data.barcode.substr(9,1)))) {
+    			var txtBox = $(".txtBox.tp2.numBox");
+    			txtBox.text(data.barcode);
+    			$('.btn.red.m.w100p').trigger('click');
+    		} else {
+    			LEMP.Window.toast({
+    				"_sMessage":"정상적인 바코드번호가 아닙니다.",
+    				'_sDuration' : 'short'
+    			});
+    	}
+}
 }
